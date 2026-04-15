f30e304e259df940b266eeb219780c81bd11de25
Fco. Fidel Ortega <159264446+DCLive72@users.noreply.github.com>
2026-04-15 17:57:36 +0200

    Add files via upload

---
+++ Mini2DEngine_02_MovimientoConCursores/Engine.cpp
@@ -0,0 +1,185 @@
+#include "Engine.h"
+
+bool Engine::Initialize(HINSTANCE hInstance, int nCmdShow)
+{
+	if (!CreateWindowClass(hInstance, nCmdShow))
+		return false;
+	if (!InitializeDirect2D())
+		return false;
+
+	return true;
+}
+
+bool Engine::CreateWindowClass(HINSTANCE hInstance, int nCmdShow)
+{
+    const wchar_t CLASS_NAME[] = L"Mini2DEngineWindow";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Mini 2D Engine",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        nullptr,
+        nullptr,
+        hInstance,
+        this // puntero a la ventana
+    );
+
+    if (!hwnd)
+        return false;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    return true;
+}
+
+bool Engine::InitializeDirect2D()
+{
+    HRESULT hr = D2D1CreateFactory(
+        D2D1_FACTORY_TYPE_SINGLE_THREADED,
+        &factory
+    );
+
+    if (FAILED(hr))
+        return false;
+
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+
+    hr = factory->CreateHwndRenderTarget(
+        D2D1::RenderTargetProperties(),
+        D2D1::HwndRenderTargetProperties(
+            hwnd,
+            D2D1::SizeU(
+                rc.right - rc.left,
+                rc.bottom - rc.top
+            )
+        ),
+        &renderTarget
+    );
+
+    if (FAILED(hr))
+        return false;
+
+    hr = renderTarget->CreateSolidColorBrush(
+        D2D1::ColorF(D2D1::ColorF::White),
+        &brush
+    );
+
+    return SUCCEEDED(hr);
+}
+
+int Engine::Run()
+{
+    MSG msg = {};
+
+    while (running)
+    {
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                running = false;
+
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        Update();
+        Render();
+    }
+
+    return 0;
+}
+
+void Engine::Update()
+{
+    // lógica del juego
+    if (keyLeft)  playerX -= playerSpeed;
+    if (keyRight) playerX += playerSpeed;
+    if (keyUp)    playerY -= playerSpeed;
+    if (keyDown)  playerY += playerSpeed;
+}
+
+void Engine::Render()
+{
+    renderTarget->BeginDraw();
+
+    renderTarget->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+
+    renderTarget->FillRectangle(
+        D2D1::RectF(
+            playerX,
+            playerY,
+            playerX + 80,
+            playerY + 80
+        ),
+        brush
+    );
+
+    renderTarget->EndDraw();
+}
+
+LRESULT CALLBACK Engine::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    Engine* engine = nullptr;
+
+    if (msg == WM_NCCREATE)
+    {
+        CREATESTRUCT* cs = reinterpret_cast<CREATESTRUCT*>(lParam);
+        engine = static_cast<Engine*>(cs->lpCreateParams);
+
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(engine));
+    }
+    else
+    {
+        engine = reinterpret_cast<Engine*>(
+            GetWindowLongPtr(hwnd, GWLP_USERDATA)
+            );
+    }
+
+    if (engine)
+    {
+        switch (msg)
+        {
+        case WM_KEYDOWN:
+            switch (wParam)
+            {
+            case VK_LEFT:  engine->keyLeft = true; break;
+            case VK_RIGHT: engine->keyRight = true; break;
+            case VK_UP:    engine->keyUp = true; break;
+            case VK_DOWN:  engine->keyDown = true; break;
+
+            case 'Q':
+            case VK_ESCAPE:
+                PostQuitMessage(0);
+                return 0;
+            }
+            break;
+
+        case WM_KEYUP:
+            switch (wParam)
+            {
+            case VK_LEFT:  engine->keyLeft = false; break;
+            case VK_RIGHT: engine->keyRight = false; break;
+            case VK_UP:    engine->keyUp = false; break;
+            case VK_DOWN:  engine->keyDown = false; break;
+            }
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        }
+    }
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
---
+++ Mini2DEngine_02_MovimientoConCursores/Engine.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <Windows.h>
+#include <d2d1.h>
+
+#pragma comment(lib, "d2d1.lib")
+
+class Engine
+{
+private:
+	HWND hwnd = nullptr;
+
+	ID2D1Factory* factory = nullptr;
+	ID2D1HwndRenderTarget* renderTarget = nullptr;
+	ID2D1SolidColorBrush* brush = nullptr;
+
+	bool running = true;
+
+	float playerX = 100.0f;
+	float playerY = 100.0f;
+	float playerSpeed = 5.0f;
+
+	bool keyLeft = false;
+	bool keyRight = false;
+	bool keyUp = false;
+	bool keyDown = false;
+
+public:
+	bool Initialize(HINSTANCE hInstance, int nCmdShow);
+	int Run();
+
+private:
+	bool CreateWindowClass(HINSTANCE hInstance, int nCmdShow);
+	bool InitializeDirect2D();
+
+	void Update();
+	void Render();
+
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+};
